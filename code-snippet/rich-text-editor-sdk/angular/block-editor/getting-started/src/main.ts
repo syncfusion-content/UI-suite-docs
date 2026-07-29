4edc4d1f24700dd136f14790ca89f00b402dd539
Gowtham-ops-SF <gowtham.seenuvasan@syncfusion.com>
2026-07-29 21:45:33 +0530

    changes committed

--- code-snippet/rich-text-editor-sdk/angular/block-editor/getting-started/src/main.ts
+++ code-snippet/rich-text-editor-sdk/angular/block-editor/getting-started/src/main.ts
@@ -1,4 +1,4 @@
-import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app';
+import { App } from './app.component';
+
 bootstrapApplication(App).catch((err) => console.error(err));
