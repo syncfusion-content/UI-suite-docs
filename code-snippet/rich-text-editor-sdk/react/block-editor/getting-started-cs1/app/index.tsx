b780cb14bf616d4f93d71438d86d4ff618312502
VikramVaratharajan-SF <vikram.varatharajan@syncfusion.com>
2026-07-29 18:31:13 +0530

    Update comment style for BlockEditor import

--- code-snippet/rich-text-editor-sdk/react/block-editor/getting-started-cs1/app/index.tsx
+++ code-snippet/rich-text-editor-sdk/react/block-editor/getting-started-cs1/app/index.tsx
@@ -1,4 +1,4 @@
-// Import the BlockEditor
+{ /* Import the BlockEditor.*/ }
 import { BlockEditorComponent } from '@syncfusion/ej2-react-blockeditor';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
